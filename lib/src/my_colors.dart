880eaed448c3187b84a47a7a3a59063ebb82187a
conghaonet <hao.cong@app2m.com>
2023-01-03 01:02:13 +0800

    add systemNavigationBarColor in MyColors

--- lib/src/my_colors.dart
+++ lib/src/my_colors.dart
@@ -3,28 +3,34 @@ import 'package:flutter/material.dart';
 @immutable
 class MyColors extends ThemeExtension<MyColors> {
   static MyColors light = MyColors(
+    systemNavigationBarColor: Colors.white,
     promptBackgroundColor: Colors.white,
     completionBackgroundColor: Colors.grey.shade200,
   );
   static const dark = MyColors(
+    systemNavigationBarColor: Colors.black,
     promptBackgroundColor: Colors.black45,
     completionBackgroundColor: Colors.black38,
   );
 
   const MyColors({
+    required this.systemNavigationBarColor,
     required this.promptBackgroundColor,
     required this.completionBackgroundColor,
   });
 
+  final Color? systemNavigationBarColor;
   final Color? promptBackgroundColor;
   final Color? completionBackgroundColor;
 
   @override
   MyColors copyWith({
+    Color? systemNavigationBarColor,
     Color? promptBackgroundColor,
     Color? completionBackgroundColor,
   }) {
     return MyColors(
+      systemNavigationBarColor: systemNavigationBarColor,
       promptBackgroundColor: promptBackgroundColor,
       completionBackgroundColor: completionBackgroundColor,
     );
@@ -36,6 +42,7 @@ class MyColors extends ThemeExtension<MyColors> {
       return this;
     }
     return MyColors(
+      systemNavigationBarColor: Color.lerp(systemNavigationBarColor, other.systemNavigationBarColor, t),
       promptBackgroundColor: Color.lerp(promptBackgroundColor, other.promptBackgroundColor, t),
       completionBackgroundColor: Color.lerp(completionBackgroundColor, other.completionBackgroundColor, t),
     );
