126f5afa4d4e1e9c38e84f54d6b9aa0aa5864466
conghaonet <hao.cong@app2m.com>
2023-01-07 16:33:31 +0800

    bug fixes

--- lib/src/page/chat_page.dart
+++ lib/src/page/chat_page.dart
@@ -94,7 +94,7 @@ class _ChatPageState extends State<ChatPage> {
         actions: [
           IconButton(
             onPressed: () {
-              context.go('/settings/gpt3');
+              context.push('/settings/gpt3');
             },
             icon: const Icon(Icons.dashboard_customize),
           ),
