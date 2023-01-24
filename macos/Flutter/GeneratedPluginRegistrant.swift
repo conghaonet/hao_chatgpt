93467862a164c12cc8928a78d23375a6d70f28c2
conghaonet <hao.cong@app2m.com>
2023-01-24 21:40:01 +0800

    create database

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import path_provider_macos
-import shared_preferences_macos
+import shared_preferences_foundation
 import sqlite3_flutter_libs
 import url_launcher_macos
 
